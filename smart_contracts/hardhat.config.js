7cd9f2ba577ced4d7766c5248e6824857f90f2fc
omkarg01 <omkargujja01@gmail.com>
2022-08-07 00:08:02 +0530

    deploy script and hardhat ready

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/gelKVdjyvXRU9A4eLAXs_QD0CskBqIAS
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url:'https://eth-goerli.g.alchemy.com/v2/gelKVdjyvXRU9A4eLAXs_QD0CskBqIAS',
+      accounts:['ad8741f075ce0d99c6cab2999c870413f156796b0fcfc29c1377dd63a5183fa1']
+    }
+  }
+}
